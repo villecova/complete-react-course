b18b015be0408cff48f740905090fdb72b36e6c3
villecova <75551900+villecova@users.noreply.github.com>
2022-10-24 00:27:58 -0500

    feat(first-commit): files added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+    
+    let msg = 'awesome';
+    
+    return (
+        <div>
+            <h2 className="app-header">This is my first {msg} App component</h2>
+            <p className="app-paragraph">This is my first {msg} App component</p>
+            <img src="./logo192.png" alt=""/>
+        </div>
+    );
+
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './App';
+
+// const header = <h1>This is my first React App</h1>
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(header);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App/>
+);
